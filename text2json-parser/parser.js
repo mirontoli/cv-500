91e1ef2b0d6b87ceebcf7443a67ce1dadc4318fb
Evgeny Belkin <sunafajro@gmail.com>
2017-12-08 00:02:26 +0300

    added text to json parser for dictionary data

---
+++ text2json-parser/parser.js
@@ -0,0 +1,36 @@
+
+const fs = require('fs');
+
+fs.readFile('./data.txt', 'utf8',  (err, data) => {
+  if (err) throw err;
+  let resultJSON = [];
+  const array = data.toString().split("\r\n");
+  if (array.length) {
+    array.forEach(row => {
+        const tempRow = row.trim().split(":");
+        if (tempRow.length) {
+          const tempExample = tempRow[3].split(';');
+          const tempJSON = {};
+          tempJSON.term = tempRow[0];
+          tempJSON.transcription = tempRow[1];
+          tempJSON.translation = tempRow[2];
+          if (tempExample.length) {
+            const parsedExamples = tempExample.map(item => {
+              const example = item.trim().split('-');
+              return {
+                cv: typeof(example[0]) === 'string' ? example[0].trim() : example[0],
+                ru: typeof(example[1]) === 'string' ? example[1].trim() : example[1]
+              };
+            });
+            tempJSON.examples = parsedExamples;
+          } else {
+            tempJSON.examples = tempRow[3];
+          }
+          resultJSON.push(tempJSON);
+        }
+    });
+  }
+  fs.writeFile('./output.json', JSON.stringify(resultJSON), error => {
+    if (error) throw error;
+  }); 
+});
