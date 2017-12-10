71fa951ae94736b7c9e4a0f87936d4374b489897
Evgeny Belkin <sunafajro@gmail.com>
2017-12-10 09:59:37 +0300

    #10 data table replaced by List component. added pagination function.

--- ReactApp/src/utils/utils.js
+++ ReactApp/src/utils/utils.js
@@ -1,38 +1,64 @@
+import React from 'react';
 import Uniqid from 'uniqid';
 
-/* метод готовит исходный массив данных с уникальными id */
-export const prepareTableData = dictionary => {
-  const dataSource = dictionary.map(item => {
-    return {
-      key: Uniqid(),
-      term: item.term,
-      transcription: item.transcription,
-      translation: item.translation,
-      examples: item.examples
-    };
-  });
+/** 
+ * метод готовит исходный массив данных с уникальными id
+ * @param {Array} dictionary
+ * @returns {Array}
+ */
+export const prepareListData = dictionary => {
+  let dataSource = [];
+  if (dictionary && dictionary.length) {
+    dataSource = dictionary.map(item => {
+      const id =  Uniqid();
+      return {
+        key: id,
+        id: id, 
+        title: item.term + ' (' + item.transcription + ')',
+        description: item.translation,
+        content: prepareExamplesData(item.examples),
+      };
+    });
+  }
   return dataSource;
 };
 
-/* метод возвращает массив отфильтрованный из исходного массива строк */
-export const filterTableData = (data, term = null) => {
+/** 
+ * метод возвращает массив отфильтрованный из исходного массива строк
+ * @param {Array} data
+ * @param {string} term
+ * @param {page} number
+ * @returns {Array}
+ */
+export const filterListData = (data, term = null, page = 1) => {
   let dataSource = [];
   if (!term) {
-    dataSource = data.map(item => {
+    const slicedData = data.slice(page * 10 - 10, page * 10);
+    dataSource = slicedData.map(item => {
       return {
-        key: Uniqid(),
-        term: item.term,
-        transcription: item.transcription,
-        translation: item.translation,
-        examples: item.examples
+        key: item.id,
+        id: item.id,
+        title: item.title,
+        description: item.description,
+        content: item.content,
       };
     });
   } else {
     /* создаем регулярку для поиска строки */
     const re = new RegExp("^" + term, "i");
     dataSource = data.filter(item => {
-      return item.term.search(re) !== -1;
+      return item.title.search(re) !== -1;
     });
   }
   return dataSource;
 };
+
+const prepareExamplesData = (examples) => {
+  if (examples && examples.length) {
+    return examples.map(item => {
+      return (<span key={Uniqid()}><b>{item.cv}</b>{item.ru ? ' - ' + item.ru : null}; </span>);
+    })
+  } else {
+    return null;
+  }
+}
