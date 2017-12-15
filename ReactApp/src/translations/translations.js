86857209cde6aecf17e90f998c0d1ee19ca71487
Evgeny Belkin <sunafajro@gmail.com>
2017-12-15 23:58:01 +0300

    added translations of header and footer

---
+++ ReactApp/src/translations/translations.js
@@ -0,0 +1,12 @@
+export const labels = {
+  pageTitle: {
+    cv: "Чӑваш чӗлхин 500 тӗп тымарӗсем",
+    ru: "500 основных корней чувашских слов",
+    eo: "500 ĉefaj radikoj de ĉuvaŝaj vortoj"
+  },
+  pageFooter: {
+    cv: "«Хавал» чăваш халăх пĕрлешĕвĕ",
+    ru: "Чувашская общественная организация «Хавал»",
+    eo: "Ĉuvaŝa socia organizo «Ĥaval»"
+  }
+};
