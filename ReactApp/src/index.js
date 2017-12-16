4026eb12efa0cf679e60c3dca19861924e9dec7b
Evgeny Belkin <sunafajro@gmail.com>
2017-12-16 19:30:32 +0300

    firebase integration was disabled

--- ReactApp/src/index.js
+++ ReactApp/src/index.js
@@ -2,18 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./components/App";
-import * as firebase from "firebase";
+// import * as firebase from "firebase";
 import registerServiceWorker from "./registerServiceWorker";
 
-const config = {
-  apiKey: "AIzaSyArKVBI6tJZqxYfm5vqFmtIqEIBwKAGjIA",
-  authDomain: "cv500ru.firebaseapp.com",
-  databaseURL: "https://cv500ru.firebaseio.com",
-  projectId: "cv500ru",
-  storageBucket: "cv500ru.appspot.com",
-  messagingSenderId: "154321306293"
-};
-firebase.initializeApp(config);
+// const config = {
+//   apiKey: "AIzaSyArKVBI6tJZqxYfm5vqFmtIqEIBwKAGjIA",
+//   authDomain: "cv500ru.firebaseapp.com",
+//   databaseURL: "https://cv500ru.firebaseio.com",
+//   projectId: "cv500ru",
+//   storageBucket: "cv500ru.appspot.com",
+//   messagingSenderId: "154321306293"
+// };
+// firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
