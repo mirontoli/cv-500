d3aa8d10abc7d62b02bcfdff83597249e3b65701
Evgeny Belkin <sunafajro@gmail.com>
2017-12-17 14:06:21 +0300

    removed unnecessary code

--- ReactApp/src/index.js
+++ ReactApp/src/index.js
@@ -2,18 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { App } from "./components/App";
-// import * as firebase from "firebase";
-import registerServiceWorker from "./registerServiceWorker";
-
-// const config = {
-//   apiKey: "AIzaSyArKVBI6tJZqxYfm5vqFmtIqEIBwKAGjIA",
-//   authDomain: "cv500ru.firebaseapp.com",
-//   databaseURL: "https://cv500ru.firebaseio.com",
-//   projectId: "cv500ru",
-//   storageBucket: "cv500ru.appspot.com",
-//   messagingSenderId: "154321306293"
-// };
-// firebase.initializeApp(config);
+//import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+//registerServiceWorker();
