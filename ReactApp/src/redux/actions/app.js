b195994336796fd36114649b3bda8c9f36d3d1c5
Evgeny Belkin <sunafajro@gmail.com>
2017-12-18 23:40:50 +0300

    added actions and reducer for app

---
+++ ReactApp/src/redux/actions/app.js
@@ -0,0 +1,83 @@
+import {
+  GET_APPSTATE,
+  GET_APPSTATE_SUCCESS,
+  GET_APPSTATE_FAILED,
+  UPDATE_APPSTATE,
+  CHANGE_APP_LANGUAGE
+} from "../constants";
+import { getNextLanguage, prepareListData } from "../../utils/utils"; 
+
+export const getState = () => {
+  return dispatch => {
+    dispatch({
+      type: GET_APPSTATE
+    });
+    fetch("/api/state", {
+      method: "POST",
+      accept: "application/json",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Произошла ошибка!");
+        }
+        return response.json();
+      })
+      .then(state => {
+        dispatch(getStateSuccess(state));
+      })
+      .catch(error => {
+        dispatch(getStateFailed(error.message));
+      });
+  };
+};
+
+export const getStateSuccess = state => {
+  return dispatch => {
+    dispatch({
+      type: GET_APPSTATE_SUCCESS,
+      data: prepareListData(state.data),
+      loggedIn: state.loggedIn,
+      language: state.language,
+      rawData: state.data,
+      user: state.user.length === undefined ? state.user : {},
+    });
+  };
+};
+
+export const getStateFailed = error => {
+  return dispatch => {
+    dispatch({
+      type: GET_APPSTATE_FAILED,
+      error
+    });
+  };
+};
+
+/* action updates state of app after user login or logout */
+export const updateAppState = newState => {
+  return dispatch => {
+    dispatch({
+      type: UPDATE_APPSTATE,
+      loggedIn: newState.loggedIn,
+      user: newState.user,
+      language: newState.language
+    });
+  };
+};
+
+/* action changes app language and filters data by it */
+export const changeAppLanguage = () => {
+  return (dispatch, getState) => {
+    const { language, rawData } = getState().app;
+    const nextLang = getNextLanguage(language);
+    dispatch({
+      type: CHANGE_APP_LANGUAGE,
+      language: nextLang,
+      data: prepareListData(rawData, nextLang)
+    });
+  };
+};
