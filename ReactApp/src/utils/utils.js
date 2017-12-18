38dffd7e7470562ef4c93d899745465745aa9f6d
Evgeny Belkin <sunafajro@gmail.com>
2017-12-18 23:03:02 +0300

    method which changes language moved to utils

--- ReactApp/src/utils/utils.js
+++ ReactApp/src/utils/utils.js
@@ -6,7 +6,7 @@ import Uniqid from 'uniqid';
  * @param {Array} dictionary
  * @returns {Array}
  */
-export const prepareListData = (dictionary, lang = 'ru') => {
+export const prepareListData = (dictionary = [], lang = 'ru') => {
   let dataSource = [];
   if (dictionary.length) {
     dictionary.forEach(item => {
@@ -30,30 +30,39 @@ export const prepareListData = (dictionary, lang = 'ru') => {
  * @param {page} number
  * @returns {Array}
  */
-export const filterListData = (data, term = null, page = 1) => {
+export const filterListData = (data = [], term = null, page = 1) => {
   let dataSource = [];
   let num = 0;
-  if (!term) {
-    dataSource = data.map(item => {
-      return {
-        key: item.id,
-        id: item.id,
-        title: item.title,
-        description: item.description,
-        content: item.content,
-        audio: item.audio,
-      };
-    });
-  } else {
-    dataSource = data.filter(item => {
-      return item.title.indexOf(term.toLocaleLowerCase()) === 0;
-    });
-  }
-  num = dataSource.length;
-  if (num > 10) {
-    dataSource = dataSource.slice(page * 10 - 10, page * 10);
+  if (data.length) {
+    if (!term) {
+      dataSource = data.map(item => {
+        return {
+          key: item.id,
+          id: item.id,
+          title: item.title,
+          description: item.description,
+          content: item.content,
+          audio: item.audio,
+        };
+      });
+    } else {
+      dataSource = data.filter(item => {
+        return item.title.indexOf(term.toLocaleLowerCase()) === 0;
+      });
+    }
+    num = dataSource.length;
+    if (num > 10) {
+      dataSource = dataSource.slice(page * 10 - 10, page * 10);
+    }
   }
   return { dataSource, num };
+};
+
+export const getNextLanguage = language => {
+  const langs = ["ru", "cv", "eo"];
+  const position = langs.indexOf(language);
+  const nextLang = langs[position + 1] ? langs[position + 1] : langs[0];
+  return nextLang;
 };
 
 const prepareTranslationByLang = (data, lang) => {
