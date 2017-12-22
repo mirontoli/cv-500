66a10074c6eeee78c6e2de815c4b8e94233d6910
Evgeny Belkin <sunafajro@gmail.com>
2017-12-22 22:51:36 +0300

    added method for fetching csrf token

--- ReactApp/src/utils/utils.js
+++ ReactApp/src/utils/utils.js
@@ -102,3 +102,28 @@ const prepareExamplesData = (examples, lang = 'ru') => {
 export const Aux = (props) => {
   return props.children;
 };
+
+export const getCsrfToken = () => {
+  return new Promise((resolve, reject) => {
+    fetch("/csrfToken", {
+      method: "GET",
+      accept: "application/json",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Произошла ошибка!");
+        }
+        return response.json();
+      })
+      .then(csrf => {
+        resolve(csrf);
+      })
+      .catch(error => {
+        reject(error.message);
+      });
+  });
+}
