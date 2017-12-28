f8a3a86c572a41af92dfbc67ddb53fb46f91d3ee
Evgeny Belkin <sunafajro@gmail.com>
2017-12-29 00:02:32 +0300

    added translations

--- client/src/translations/translations.js
+++ client/src/translations/translations.js
@@ -2,36 +2,66 @@ export const labels = {
   pageTitle: {
     cv: "Чӑваш чӗлхин 500 тӗп тымарӗсем",
     ru: "500 основных корней чувашских слов",
-    eo: "500 ĉefaj radikoj de ĉuvaŝaj vortoj"
+    eo: "500 ĉefaj radikoj de ĉuvaŝaj vortoj",
   },
   pageFooter: {
     cv: "«Хавал» чăваш халăх пĕрлешĕвĕ",
     ru: "Чувашская общественная организация «Хавал»",
-    eo: "Ĉuvaŝa socia organizo «Ĥaval»"
+    eo: "Ĉuvaŝa socia organizo «Ĥaval»",
   },
   menuLoginButton: {
     cv: "Кӗр",
     ru: "Вход",
-    eo: "Eniri"
+    eo: "Eniri",
   },
   menuLogoutButton: {
     cv: "Тух",
     ru: "Выйти",
-    eo: "Eliri"
+    eo: "Eliri",
   },
   formUsernameField: {
     cv: 'Имя пользователя',
     ru: 'Имя пользователя',
-    eo: 'Uzanto nomo'
+    eo: 'Uzanto nomo',
   },
   formPasswordField: {
     cv: 'Вăрттăн сăмах',
     ru: 'Пароль',
-    eo: 'Pasvorto'
+    eo: 'Pasvorto',
   },
   formLoginButton: {
     cv: 'Кӗр',
     ru: 'Войти',
-    eo: 'Eniri'
+    eo: 'Eniri',
+  },
+  formTermField: {
+    cv: 'Сӑмах',
+    ru: 'Слово',
+    eo: 'Vorto',
+  },
+  formTranscriptionField: {
+    cv: 'Траскрипция',
+    ru: 'Траскрипция',
+    eo: 'Vorto',
+  },
+  formAddExampleButton: {
+    cv: 'Добавить',
+    ru: 'Добавить',
+    eo: 'Aldoni',
+  },
+  formSaveExampleButton: {
+    cv: 'Сохранить',
+    ru: 'Сохранить',
+    eo: 'Savi',
+  },
+  formAddExampleError: {
+    cv: 'Необходимо заполниить все поля',
+    ru: 'Необходимо заполниить все поля',
+    eo: 'Indas pleni ĉiun kampon',
+  },
+  formAddExampleNew: {
+    cv: 'Ҫӗнӗ пример',
+    ru: 'Новый пример',
+    eo: 'Nova ekzemplo',
   }
 };
