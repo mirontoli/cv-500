23176f986fd54bc7bc9cdefb6aca851c3e380343
Evgeny Belkin <sunafajro@gmail.com>
2018-01-14 01:22:01 +0300

    added methods for handling create/update actions

--- server/api/controllers/ArticleController.js
+++ server/api/controllers/ArticleController.js
@@ -6,6 +6,35 @@
  */
 
 module.exports = {
-	
+  create: (req, res) => {
+    if (req.session.loggedIn) {
+      const article = Object.assign({}, req.body.Article);
+      delete article.id;
+      Article.createArticle(article)
+        .then(result => {
+          return res.send({ result });
+        })
+        .catch(error => {
+          return res.status(error.status).send(error.message);
+        });
+    } else {
+      return res.status(401).send("User is not authorized for thos action");
+    }
+  },
+  update: (req, res) => {
+    if (req.session.loggedIn) {
+      const id = req.body.Article.id;
+      const article = Object.assign({}, req.body.Article);
+      delete article.id;
+      Article.updateArticle(article, id)
+        .then(result => {
+          return res.send({ result });
+        })
+        .catch(error => {
+          return res.status(error.status).send(error.message);
+        });
+    } else {
+      return res.status(401).send("User is not authorized for thos action");
+    }
+  }
 };
-
